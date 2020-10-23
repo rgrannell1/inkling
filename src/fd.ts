b0e9a2dafdeacd331b2e91a3af54adaf7d1b5ab4
Róisín Grannell <r.grannell2@gmail.com>
2020-10-23 21:14:17 +0100

    removed unused code

--- src/fd.ts
+++ src/fd.ts
@@ -1,69 +1,16 @@
 
-import { EventEmitter } from 'events'
 import through from 'through'
-
-const emitStream = (emitter:EventEmitter) => {
-	return emitter
-}
 
 interface StdoutArgs {
 	rows: number
 	columns: number
 }
 
-class Stdout extends EventEmitter {
-	rows:number
-	columns:number
-	constructor (args:any) {
-		super(args)
-		this.rows = args.rows
-		this.columns = args.columns
-	}
-
-	readonly frames: string[] = []
-	private _lastFrame?: string
-
-	write = (frame: string) => {
-		this.frames.push(frame)
-		this._lastFrame = frame
-	}
-
-	lastFrame = () => {
-		return this._lastFrame
-	}
-}
-
-class Stdin extends EventEmitter {
-  isTTY = true
-  write (data:string) {
-    this.emit('data', data)
-  }
-  setEncoding () { }
-  setRawMode () { }
-  result () { }
-	pause () { }
-	pipe (stream:any) {
-
-	}
-}
-
-class Stderr extends EventEmitter {
-	readonly frames: string[] = []
-	private _lastFrame?: string
-
-	write = (frame: string) => {
-		this.frames.push(frame)
-		this._lastFrame = frame
-	}
-
-	lastFrame = () => {
-		return this._lastFrame
-	}
-}
-
 interface StubStdout extends through.ThroughStream {
 	lastFrame? (): string | undefined
 	_lastFrame?: string
+	rows?: number
+	columns?: number
 }
 
 export const stubStdout = (args:StdoutArgs) => {
@@ -72,6 +19,8 @@ export const stubStdout = (args:StdoutArgs) => {
 		ref._lastFrame = data
 	})
 	ref.lastFrame = () => ref._lastFrame
+	ref.columns = args.columns
+	ref.rows = args.rows
 
 	return ref
 }
